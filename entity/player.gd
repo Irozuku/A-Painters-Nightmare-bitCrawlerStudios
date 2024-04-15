79228d314282f5f33a70db913b0ff4a9b0165356
Irozuku <emiliodu@gmail.com>
2024-04-15 17:43:17 -0400

    Added basic attack logic and his possible asset to a folder
    Created the basic automatic attack for the character, which triggers every 1 second. Currently, it's just a Godot icon.

--- entity/player.gd
+++ entity/player.gd
@@ -1,5 +1,8 @@
 extends "res://entity/EntityBase.gd"
 
+@export var BASIC_ATTACK: PackedScene = preload("res://projectiles/basic_attack.tscn")
+@onready var basic_attack_timer = $BasicAttackTimer
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -9,5 +12,18 @@ func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	# Get velocity
 	velocity = direction * SPEED
-
 	move_and_slide()
+	
+	if basic_attack_timer.is_stopped():
+		basic_attack(self.global_position.direction_to(get_global_mouse_position()))
+
+func basic_attack(att_direction: Vector2):
+	if BASIC_ATTACK:
+		var att = BASIC_ATTACK.instantiate()
+		get_tree().current_scene.add_child(att)
+		att.global_position = self.global_position
+		
+		var att_rotation = att_direction.angle()
+		att.rotation = att_rotation
+		
+		basic_attack_timer.start()
---
+++ projectiles/basic_attack.gd
@@ -0,0 +1,16 @@
+extends "res://overlap/Hitbox.gd"
+
+@export var SPEED: int = 200
+
+func _physics_process(delta):
+	var direction = Vector2.RIGHT.rotated(rotation)
+	global_position += SPEED * direction * delta
+
+
+func destroy():
+	queue_free()
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free()
+
