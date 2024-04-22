0094d9496632ba055d7cbae91ff420df19303e71
Sebastian Saez <sebastian.saez@ug.uchile.cl>
2024-04-22 14:49:48 -0400

    Fixed change during draw

--- scenes/paint_bar.gd
+++ scenes/paint_bar.gd
@@ -4,7 +4,7 @@ extends Area2D
 @onready var BlueBorder = $BluePaint/BlueBorder
 @onready var YellowBorder = $YellowPaint/YellowBorder
 
-@onready var PaintList = [RedBorder, BlueBorder, YellowBorder]
+@onready var paint_array = [RedBorder, BlueBorder, YellowBorder]
 @onready var current_index
 
 @onready var active = load("res://assets/border_sprite/border_on.png")
@@ -17,32 +17,33 @@ func _ready():
 
 # Change the active paint color
 func change_active(paint):
-	for border in PaintList:
+	for border in paint_array:
 		border.texture = inactive
 	current_index = paint
-	PaintList[current_index].texture = active
+	paint_array[current_index].texture = active
 
 func shift_active_right():
 	current_index = (current_index+1)%3
-	for border in PaintList:
+	for border in paint_array:
 		border.texture = inactive
-	PaintList[current_index].texture = active
+	paint_array[current_index].texture = active
 	
 func shift_active_left():
 	current_index = (current_index-1)%3
-	for border in PaintList:
+	for border in paint_array:
 		border.texture = inactive
-	PaintList[current_index].texture = active
+	paint_array[current_index].texture = active
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (Input.is_action_just_pressed("red_paint")):
-		change_active(0)
-	if (Input.is_action_just_pressed("blue_paint")):
-		change_active(1)
-	if (Input.is_action_just_pressed("yellow_paint")):
-		change_active(2)
-	if (Input.is_action_just_pressed("change_paint_left")):
-		shift_active_left()
-	if (Input.is_action_just_pressed("change_paint_right")):
-		shift_active_right()
+	if not (Input.is_action_pressed("left_click")):
+		if (Input.is_action_just_pressed("red_paint")):
+			change_active(0)
+		elif (Input.is_action_just_pressed("blue_paint")):
+			change_active(1)
+		elif (Input.is_action_just_pressed("yellow_paint")):
+			change_active(2)
+		elif (Input.is_action_just_pressed("change_paint_left")):
+			shift_active_left()
+		elif (Input.is_action_just_pressed("change_paint_right")):
+			shift_active_right()
