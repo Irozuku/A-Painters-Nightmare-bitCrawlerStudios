85c716320acd436db3db41924c4a650f220d2feb
Sebastian Saez <sebastian.saez@ug.uchile.cl>
2024-04-22 20:22:24 -0400

    added release power functions

--- scenes/grid.gd
+++ scenes/grid.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 var points = []
+var loaded_powers = []
 var active_paint
 
 @onready var text = $Label
+
 @onready var sprite1 = $Point1/PointSprite1
 @onready var sprite2 = $Point2/PointSprite2
 @onready var sprite3 = $Point3/PointSprite3
@@ -18,19 +20,24 @@ var active_paint
 func _ready():
 	pass # Replace with function body.
 
+func load_power(power: String, paint: int):
+	if loaded_powers.is_empty():
+		loaded_powers.append([power, paint])
+	else:
+		for pow in loaded_powers:
+			if not (power in pow):
+				loaded_powers.append([power, paint])
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (Input.is_action_just_released("left_click")):
-		if (points == []):
-			text.text = "Nada"
-		elif (points == [1, 5, 9, 6, 3, 7] or points == [7, 5, 3, 6, 9, 1]):
-			text.text = "Poder 1"
+		active_paint = $PaintBar.current_index
+		if (points == [1, 5, 9, 6, 3, 7] or points == [7, 5, 3, 6, 9, 1]):
+			load_power("Poder 1", active_paint)
 		elif (points == [1, 2, 3, 5, 7, 8, 9] or points == [9, 8, 7, 5, 3, 2, 1]):
-			text.text = "Poder 2"
+			load_power("Poder 2", active_paint)
 		elif (points == [4, 8, 5, 2, 6] or points == [6, 2, 5, 8, 4]):
-			text.text = "Poder 3"
-		else:
-			text.text = "Incorrecto"
+			load_power("Poder 3", active_paint)
 		sprite1.modulate = Color("ffffff")
 		sprite2.modulate = Color("ffffff")
 		sprite3.modulate = Color("ffffff")
@@ -41,3 +48,6 @@ func _process(delta):
 		sprite8.modulate = Color("ffffff")
 		sprite9.modulate = Color("ffffff")
 		points.clear()
+	if (Input.is_action_pressed("release_power")):
+		#Envia la orden de que poder realizar a ...
+		loaded_powers.clear()
