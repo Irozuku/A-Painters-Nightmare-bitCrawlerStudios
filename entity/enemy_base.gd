1522c48e2d419af9ac91f6df7d12c19374720b80
Gutixdxd <i.gutierrez@ing.uchile.cl>
2024-05-06 13:10:27 -0400

    Enemy
    Enemy Chases player constantly

--- entity/enemy_base.gd
+++ entity/enemy_base.gd
@@ -1,7 +1,23 @@
-extends "res://entity/EntityBase.gd"
+extends CharacterBody2D
+
+class_name Enemy
+
+var type
+var speed = 200
+var target = Vector2()
+var acceleration = 1000
+
+	
+	
+func _ready():
+	target = get_node("/root/Main/Player")
 
 func _physics_process(delta):
-	pass
+	var direction_x = global_position.direction_to(target.global_position).x
+	var direction_y = global_position.direction_to(target.global_position).y
+	velocity.x = move_toward(velocity.x, direction_x * speed, acceleration * delta)
+	velocity.y = move_toward(velocity.y, direction_y * speed, acceleration * delta)
+	move_and_slide()
 	
 func die():
 	queue_free()
