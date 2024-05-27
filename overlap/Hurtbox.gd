87b4a95ec940b41b9ea4f60e0d89c61602165765
Gutixdxd <i.gutierrez@ing.uchile.cl>
2024-05-27 12:34:05 -0400

    Enemy does damage

--- overlap/Hurtbox.gd
+++ overlap/Hurtbox.gd
@@ -4,6 +4,27 @@ extends Area2D
 @export_enum("Cooldown", "HitOnce", "DisableHitbox") var HurtboxType = 0
 
 @onready var collision = $CollisionShape2D
-#@onready var disableTimer = $DisableTimer
+@onready var disableTimer = $DisableTimer
 
 signal hurt(damage)
+
+
+
+func _on_area_entered(area):
+	if area.is_in_group("attack"):
+		if not area.get("damage") == null :
+			match HurtboxType:
+				0: #Cooldown
+					collision.call_deferred("set", "disabled", true)
+					disableTimer.start()
+				1: #Hitonce
+					pass
+				2: #Disable Hitbox
+					if area.has_method("tempdisable"):
+						area.tempdisable()
+			var damage = area.damage
+			emit_signal("hurt", damage)
+
+
+func _on_disable_timer_timeout():
+	collision.call_deferred("set", "disabled", false)
