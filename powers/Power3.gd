5f030e330b77a571fe65e420f3d2f1795a7b4ca1
Sebastian Saez <sebastian.saez@ug.uchile.cl>
2024-06-03 17:01:15 -0400

    added power 2 code

--- powers/Power3.gd
+++ powers/Power3.gd
@@ -3,15 +3,6 @@ extends Node2D
 var speed: int = 200
 @export var damage = 10
 
-@onready var ray1 = $Ray1
-@onready var sprite1 = $Line1
-@onready var ray2 = $Ray2
-@onready var sprite2 = $Line2
-@onready var ray3 = $Ray3
-@onready var sprite3 = $Line3
-@onready var ray4 = $Ray4
-@onready var sprite4 = $Line4
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Crear un temporizador para eliminar el nodo después de 2 segundos
@@ -24,24 +15,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	ray1.target_position += speed * Vector2.RIGHT * delta
-	ray2.target_position += speed * Vector2.LEFT * delta
-	ray3.target_position += speed * Vector2.UP * delta
-	ray4.target_position += speed * Vector2.DOWN * delta
-	sprite1.clear_points()
-	sprite2.clear_points()
-	sprite3.clear_points()
-	sprite4.clear_points()
-	sprite1.add_point(Vector2(0,0))
-	sprite2.add_point(Vector2(0,0))
-	sprite3.add_point(Vector2(0,0))
-	sprite4.add_point(Vector2(0,0))
-	sprite1.add_point(ray1.target_position)
-	sprite2.add_point(ray2.target_position)
-	sprite3.add_point(ray3.target_position)
-	sprite4.add_point(ray4.target_position)
-
-
+	pass
 
 func _on_timeout():
 	queue_free()  # Auto eliminar el nodo
--- powers/power_2.gd
+++ powers/power_2.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-@export var damage = 10
+@onready var ColPolygon = $Hitbox/CollisionPolygon2D
+@onready var sprite = $Sprite2D
+
+var speed: float = 400
+var sprite_speed: float = 5.0
+var radio_inicial: float = 20.0
+var radio_actual: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,8 +17,26 @@ func _ready():
 	add_child(timer)
 	timer.connect("timeout", Callable(self, "_on_timeout"))
 	timer.start()
+	radio_actual = radio_inicial
+	_update_collision_polygon()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	radio_actual += speed * delta
+	_update_collision_polygon()
+	_update_sprite(delta)
+
+func _update_collision_polygon():
+	var points = []
+	var num_segments = 64
+	for i in range(num_segments):
+		var angle = i * PI * 2 / num_segments
+		points.append(Vector2(cos(angle), sin(angle)) * (radio_actual/2))
+	ColPolygon.polygon = PackedVector2Array(points)
+
+func _update_sprite(delta):
+	sprite.scale += Vector2(sprite_speed, sprite_speed) * delta
+
+func _on_timeout():
+	queue_free()  # Auto eliminar el nodo
