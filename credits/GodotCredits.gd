a66cb2ad0825213bdbbe2bc0995f0420a083a699
Irozuku <emiliodu@gmail.com>
2024-06-29 01:57:48 -0400

    Fixed music not playing in credits

--- credits/GodotCredits.gd
+++ credits/GodotCredits.gd
@@ -71,6 +71,7 @@ var credits = [
 func _ready():
 	colorrect.color = bg_color
 	videoplayer.set_stream(Video)
+	MainMenuBgm.play_bgm_music(-15.5)
 	if !Use_Video_Audio:
 		var stream = AudioStreamPlayer.new()
 		stream.set_stream(Music)
---
+++ music/main_menu/main_menu_bgm.gd
@@ -0,0 +1,14 @@
+extends AudioStreamPlayer2D
+
+const bgm_music = preload("res://music/main_menu/11 - Sergio V. Quintero - Sorrows Embrace (Loop).mp3")
+
+func _play_music(music: AudioStream, volume):
+	if stream == music:
+		return
+	stream = music
+	volume_db = volume
+	play()
+	
+func play_bgm_music(volume = 0.0):
+	_play_music(bgm_music, volume)
+	
