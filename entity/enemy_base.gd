3b755d0048c4489547f4bf98fd2797d7efded767
Irozuku <emiliodu@gmail.com>
2024-06-30 17:26:07 -0400

    Refactored Enemy class

--- entity/enemy_base.gd
+++ entity/enemy_base.gd
@@ -32,15 +32,7 @@ func _physics_process(delta):
 	flip_sprite(direction_x)
 	
 func flip_sprite(direction_x):
-	# Rotación del sprite según la dirección horizontal
-	if direction_x < 0:
-		sprite.flip_h = true  # Voltea el sprite hacia la izquierda
-		hitbox_collision_shape.position.x = abs(hitbox_collision_shape.position.x) * -1
-		hurtbox_collision_shape.position.x = abs(hurtbox_collision_shape.position.x) * -1
-	elif direction_x > 0:
-		hitbox_collision_shape.position.x = abs(hitbox_collision_shape.position.x)
-		hurtbox_collision_shape.position.x = abs(hurtbox_collision_shape.position.x)
-		sprite.flip_h = false   # Deja el sprite sin voltear hacia la derecha
+	pass
 
 func _on_power1_collision(obj, damage):
 	# Emitir la señal de daño (o manejar el daño directamente)
@@ -56,23 +48,9 @@ func _on_blue_freeze(obj, time):
 		self.frozen = false
 
 func die():
-	print("DEAD")
-	playback.travel("dead")
-	SPEED = 0
-	hitbox_collision_shape.set_deferred("disabled", true)
-	hurtbox_collision_shape.set_deferred("disabled", true)
-	var timer = Timer.new()
-	add_child(timer)
-	timer.wait_time = animation_player.current_animation_length
-	timer.one_shot = true
-	timer.connect("timeout", _on_dead_animation_finished)
-	timer.start()
-
-func _on_dead_animation_finished():
 	queue_free()
 
 func _on_body_entered(body:Node2D):
 	queue_free()
 
-func _on_hp_changed(new_hp):
-	playback.travel("damage_taken")
+
