d20c4f333f9fde4e7dd55cf7a5469eacb8ec0df7
Irozuku <emiliodu@gmail.com>
2024-07-12 21:50:38 -0400

    Changed cursor

--- cursor/static_cursor.gd
+++ cursor/static_cursor.gd
@@ -1,10 +1,12 @@
-extends Area2D
+extends Node2D
 
 const RADIUS = 250
 var mouse_clicked
 var player_position
 var on_screen = true
 var player_to_cursor
+@onready var on_screen_cursor = $OnScreenCursor
+@onready var off_screen_cursor = $OffScreenCursor
 
 
 func _physics_process(delta):
@@ -12,14 +14,19 @@ func _physics_process(delta):
 	if on_screen:
 		global_position = mouse_clicked
 	else:
-		global_position = RADIUS * player_position.direction_to(mouse_clicked) + player_position
+		# global_position = RADIUS * player_position.direction_to(mouse_clicked) + player_position
+		off_screen_cursor.global_position = RADIUS * player_position.direction_to(mouse_clicked) + player_position
 
 
 func _on_visible_on_screen_notifier_2d_screen_entered():
 	on_screen = true
+	on_screen_cursor.show()
+	off_screen_cursor.hide()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	on_screen = false
+	on_screen_cursor.hide()
+	off_screen_cursor.show()
 
 func destroy():
 	queue_free()
