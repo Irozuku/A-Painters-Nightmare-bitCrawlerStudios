21bff3e73e8c5519d5748e9bf3b136dfd2552963
Sebastian Saez <sebastian.saez@ug.uchile.cl>
2024-07-15 17:22:47 -0400

    Removed deprecated code

--- scenes/grid.gd
+++ scenes/grid.gd
@@ -8,16 +8,6 @@ var active_paint
 @onready var text = $Label
 @onready var remainder = $SymbolRemainder
 
-@onready var sprite1 = $Point1/PointSprite1
-@onready var sprite2 = $Point2/PointSprite2
-@onready var sprite3 = $Point3/PointSprite3
-@onready var sprite4 = $Point4/PointSprite4
-@onready var sprite5 = $Point5/PointSprite5
-@onready var sprite6 = $Point6/PointSprite6
-@onready var sprite7 = $Point7/PointSprite7
-@onready var sprite8 = $Point8/PointSprite8
-@onready var sprite9 = $Point9/PointSprite9
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -50,15 +40,6 @@ func _process(delta):
 			load_power("Poder 2", active_paint)
 		elif (points == [4, 8, 5, 2, 6] or points == [6, 2, 5, 8, 4]):
 			load_power("Poder 3", active_paint)
-		sprite1.modulate = Color("ffffff")
-		sprite2.modulate = Color("ffffff")
-		sprite3.modulate = Color("ffffff")
-		sprite4.modulate = Color("ffffff")
-		sprite5.modulate = Color("ffffff")
-		sprite6.modulate = Color("ffffff")
-		sprite7.modulate = Color("ffffff")
-		sprite8.modulate = Color("ffffff")
-		sprite9.modulate = Color("ffffff")
 		points.clear()
 	if (Input.is_action_just_pressed("release_power")):
 		print("Sending powers to controler")
